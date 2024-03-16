2d0c8bd835a900303e23aca4a9c18a299c2f5424
zephyrus <joelogira@gmail.com>
2024-03-16 23:29:46 +0300

    Modified the file

--- ifelse.sh
+++ ifelse.sh
@@ -1,3 +1,11 @@
+#!/bash/bin
+
+######################################################
+#
+# This script is for loops
+#
+######################################################
+
 a=2
 b=3
 
@@ -7,3 +15,21 @@ then
 else
 	echo "b is greater than a"
 fi
+
+# Iterations
+# this is much better as compare to above for loop
+echo "Using for loop method # 2... "
+for i in $(seq 1 10); do
+    echo $i;
+done
+echo ""
+ 
+# use of while loop
+echo "Using while loop..."
+j=1
+while [ $j -le 10 ]
+do
+   echo -n "$j "
+   j=$(( j + 1 )) # increase number by 1 
+done
+echo ""
