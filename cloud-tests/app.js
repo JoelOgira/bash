8a1572330189cda82f4a5323d606ef195710fdcb
zephyrus <joelogira@gmail.com>
2024-03-17 16:30:25 +0300

    Created app.js file

---
+++ cloud-tests/app.js
@@ -0,0 +1,7 @@
+// This script prints numbers divisible by 3 and 5 but not by 15;
+
+for (let i = 0; i <= 50; i++) {
+    if ((i % 3 === 0 || i % 5 === 0) && (i % 15 !== 0)) {
+        console.log(i)
+    }
+}
