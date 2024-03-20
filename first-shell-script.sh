d70079bfb3e79cfd829a9d836ea87d743ba1dc6e
zephyrus <joelogira@gmail.com>
2024-03-20 23:35:27 +0300

    Modified the file

--- first-shell-script.sh
+++ first-shell-script.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # Create a directory
-rmdir -r cloud-tests
 mkdir cloud-tests
 
 # Create files
 cd cloud-tests
 touch index.html style.css app.js
 
-# Delete the directory
-rmdir -r cloud-tests
+
